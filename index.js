96b3267ae84fd4e0f83506e2f787be6ee8673c3f
AndriantoAGun <andrianto22491@gmail.com>
2019-07-22 22:12:02 +0700

    Tugas 16 JavaScript
    tugas Niomic tentang JavaScript meliputi Math.min dan Math.max

---
+++ index.js
@@ -0,0 +1,10 @@
+var min1 = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+var max1 = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+var min2 = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+var max2 = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+
+console.log("Nilai Minimal : " + min1);
+console.log("Nilai Maksimal : " + max1);
+
+console.log("Nilai Minimal : " + min2);
+console.log("Nilai Maksimal : " + max2);
